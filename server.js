d87471b08adb6c97436a88c9ccaf73187e2843fb
Amey <ameytotawar@gmail.com>
2017-05-09 11:10:38 +0000

    dependencies

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require("express");
-var app = express()
+var path = require("path");
+var routes = require("./api/routes");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/api', routes);
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!');
 })
